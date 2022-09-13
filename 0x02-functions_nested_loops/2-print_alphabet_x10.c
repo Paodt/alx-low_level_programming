d502d8d394375791f23d0c4d24a520d7c94e6a3d
Paodt <odehpatrick255@gmail.com>
2022-09-13 04:01:54 -0700

    first 5

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * description: program that prints lower case alphabet x10
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int j;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
