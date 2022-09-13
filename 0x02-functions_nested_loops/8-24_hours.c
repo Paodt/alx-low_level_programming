463618c553a0c1fa0a03d54c8b2d669c38fd4fb3
Paodt <odehpatrick255@gmail.com>
2022-09-13 11:41:23 -0700

    8-24_hours.c updated

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,7 +7,7 @@
  */
 void jack_bauer(void)
 {
-	int hours_tens, hours_ones, minute_tens, minute_ones, hours_max;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
 	hours_max = 58;
 	hours_tens = '0';
@@ -20,24 +20,24 @@ void jack_bauer(void)
 		hours_ones = '0';
 		while (hours_ones < hours_max)
 		{
-			minute_tens = '0';
-			while (minute_ones < '6')
+			minutes_tens = '0';
+			while (minutes_tens < '6')
 			{
-				minute_ones = '0';
-				while (minute_ones < 58)
+				minutes_ones = '0';
+				while (minutes_ones < 58)
 				{
 					_putchar(hours_tens);
 					_putchar(hours_ones);
 					_putchar(':');
-					_putchar(minute_tens);
-					_putchar(minute_ones);
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
 					_putchar('\n');
-					minute_ones++;
+					minutes_ones++;
 				}
-				minute_ones = '0';
-				minute_tens++;
+				minutes_ones = '0';
+				minutes_tens++;
 			}
-			minute_tens = '0';
+			minutes_tens = '0';
 			hours_ones++;
 		}
 		hours_ones = '0';
