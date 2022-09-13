11a9ff2610c51d128062736f35978aca07542858
Paodt <odehpatrick255@gmail.com>
2022-09-13 04:41:35 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.c"
+
+/**
+ * print_sign - Entry
+ * @n: value to return
+ * description: function that writes the sign of a number
+ * Return: 1 if true 0 otherwise
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar("0");
+		return (0);
+	}
+	else
+	{
+		_putchar("-");
+		return (-1);
+	}
+}
