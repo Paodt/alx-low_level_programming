d09fd807ef22843c01d8a9e5843114976e06d6f6
Paodt <odehpatrick255@gmail.com>
2022-09-15 04:31:48 -0700

    1 to 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int _multiply;
+
+	_multiply = a * b;
+	return (_multiply);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers
+ * Return: 0
+ * @i: int value
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i > 10; i++)
+	{
+		_putchar("%d", i);
+	}
+	_putchar('\n');
+	return (0);
+}
