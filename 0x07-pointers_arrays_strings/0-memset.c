b280369d97619dc633c276e93413b72a34d350e3
Paodt <odehpatrick255@gmail.com>
2022-09-26 04:05:09 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte
+ * @*s: memory space to be filled
+ * @b: bytes to fill
+ * @n: number of spaces to fill
+ * Return: returns pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains functions and programs including header files on the project "even more pointers, arrays and strings
