74a2e5ad37ebe1d2568ca143f2112e360aae0482
Paodt <odehpatrick255@gmail.com>
2022-09-28 05:43:49 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of string
+ * @s: string
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
