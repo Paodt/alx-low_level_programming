e73b44992eb028370d07f400e1a608491f6d66de
Paodt <odehpatrick255@gmail.com>
2022-10-01 06:28:53 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,8 @@
  * @argv: argument vector
  * Return: 0
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	while (*argv--)
-		printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
