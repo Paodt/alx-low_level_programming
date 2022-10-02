89b8b3046ea8e50e2ac12a6b04817a2ab5ab2203
Paodt <odehpatrick255@gmail.com>
2022-10-01 23:58:00 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - program that add numbers
+ * argc: argument count
+ * argv: argument vector
+ * Return: 0 if no number is passed
+ */
+int main(int argc, char *argv[])
+{
+	int i, g, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (g = 0; argv[i][g] != '\0'; g++)
+		{
+			if(!isdigit(argv[i][g]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+	
+		sum += atoi(argv[i]);
+		}
+	}
+		printf("%d\n", sum);
+	return (0);
+}
