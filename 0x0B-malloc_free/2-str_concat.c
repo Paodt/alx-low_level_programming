987d054d3c121f122ac00335d2708e74f8f32ee1
Paodt <odehpatrick255@gmail.com>
2022-10-04 01:22:51 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *str_concat - function that concatenates two strings
+ * @s1: first sring
+ * @s2: second string
+ * Return: a pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int a, b, c, limit;
+	char *strout;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (a = 0; s1[a] != '\0'; a++)
+		;
+	for (b = 0; s2[b] != '\0'; b++)
+		;
+	strout = malloc(sizeof(char) * (a + b + 1));
+
+	if (strout == NULL)
+	{
+		free(strout);
+		return (NULL);
+	}
+
+	for (c = 0; c < a; c++)
+		strout[c] = s1[c];
+	limit = b;
+	for (b = 0; b <= limit; c++ b++)
+		strout[c] = s2[b];
+	return (strout);
+}
