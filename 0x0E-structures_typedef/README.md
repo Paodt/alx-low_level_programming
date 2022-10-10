1c399bb8d26960a1d1b583e887180890a8975f50
Paodt <odehpatrick255@gmail.com>
2022-10-10 10:49:08 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contain header files, structures, functions and programs on 0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct declaration
+ * @name: first variable
+ * @age: second variable
+ * @owner: third
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
