b79571371c526e1e219cf825b3622dd038a03773
Paodt <odehpatrick255@gmail.com>
2022-10-12 07:12:54 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that executes a function given as a parameter
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
